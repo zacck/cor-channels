d195487dfbf8c0c16392381bfcbcadf0708304a7
Zacck Osiemo <coderv63@gmail.com>
2023-05-23 13:30:03 +0200

    Update implementation to use a VecDeque which acts like a ring buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,45 @@
 use std::sync::{Arc, Condvar, Mutex};
+use std::collections::VecDeque
 
 pub struct Sender<T> {
-    inner: Arc<Mutex<Inner<T>>,
+    inner: Arc<Inner<T>>,
+}
+
+impl<T> Sender<T> {
+    pub fn send(&mut self, t: T) {
+        //take the lock
+        let queue = self.inner.queue.lock().unwrap();
+        //add to the queue
+        queue.push_back(t);
+    }
 }
 
 pub struct Receiver<T> {
-    inner: Arc<Mutex<Inner<T>>>,
+    inner: Arc<Inner<T>>,
 }
 
+impl<T> Receiver<T> {
+    pub fn recv(&mut self) -> T {
+        //take the lock
+        let queue = self.inner.queue.lock().unwrap();
+        //take fromto the queue
+        queue.pop_front();
+    }
+}
+//shared items in the channel
+//basically a queue
+// where the sender puts data and
+//the receiver takes data
 struct Inner<T> {
-    queue: Vec<T>,
+    queue: Mutex<VecDeque<T>>,
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let inner = Inner { queue: Vec::new() };
+    let inner = Inner {
+        queue: Mutex::default(),
+    };
 
-    let inner = Arc::new(Mutex::new(inner));
+    let inner = Arc::new(inner);
     (
         Sender {
             inner: inner.clone(),
